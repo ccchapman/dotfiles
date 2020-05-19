dbbde17dd28b3175513f50156c2d517836bf74ba
John Doe <42548502+ccchapman@users.noreply.github.com>
2020-05-19 16:12:24 -0700

    Use Vi mode in Fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,6 @@
+alias g="git"
 export FZF_DEFAULT_COMMAND='rg --hidden --no-ignore -l -g "!.git" -g "!vendor" -g "!node_modules" ""'
+export FZF_DEFAULT_OPTS='--preview="cat {}" --height=40%'
+
+fish_vi_key_bindings
+set fish_greeting
